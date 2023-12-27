0ad09d2cec4cbac23c9da6e2ec48e9a1caa1e7c4
utqkaba <utqkaba@gmail.com>
2023-12-27 15:45:18 +0300

    books api entegration

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 p-4 text-white text-center fixed bottom-0 w-full font-extralight text-sm">
+    <footer className="bg-gray-900 p-4 text-white text-center bottom-0 w-full font-extralight text-sm">
       <p>&copy; BOOKSTORE </p>
       <div className="border-t border-gray-500 m-4"></div>
       <div className="grid grid-cols-2">
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Details = () => {
-  const [users, setUsers] = useState([]);
+  const [books, setBooks] = useState([]);
+  // secilen kitabin idsi prop olarak gelicek ona gore istek aticaz. 
+  // const [books, setBooks] = useState([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => setUsers(response.data))
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers&filter=paid-ebooks&key=AIzaSyBOYuYNIW3OSKWicmtfJ_UPfAAIlJdNFtE")
+      .then((response) => setBooks(response.data.items))
       .catch(err => {
         setError(err.message);
       });
@@ -22,13 +24,39 @@ const Details = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-col items-center justify-center border border-red-500 m-4 p-4">
-        <h2 className="text-2xl font-bold mb-4">{users.userId}</h2>
-        <p className="text-gray-600 mb-2">{users.title}</p>
-        <p className="text-gray-600 mb-2">{users.body}</p>
+      <div className="flex flex-col items-center justify-center m-4 p-4">
+        {
+          books.map((item) => {
+            const thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+            const amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+            const desc = item.volumeInfo.description;
+            const authors = item.volumeInfo.authors;
+            const publisher = item.volumeInfo.publisher;
+            const publishedDate = item.volumeInfo.publishedDate;
+
+            publisher
+            if (thumbnail != undefined && amount != undefined) {
+              return (
+                <>
+                  <div>
+                    <img className="h-64 w-64" src={thumbnail} alt="BookImage" />
+                    <div className="border-4 border-red-500 p-4 m-4">
+                      <h3 className="text-2xl font-bold"> {item.volumeInfo.title} </h3>
+                      <p> <strong>Authors:</strong> {authors} </p>
+                      <p> <strong>Publisher:</strong> {publisher} </p>
+                      <p> <strong>Published Date:</strong> {publishedDate} </p>
+                      <p> <strong>Description:</strong> {desc} </p>
+                      <p> <strong>Price:</strong> ${amount} </p>
+                    </div>
+                  </div>
+                </>
+              )
+            }
+
+          })
+        }
       </div>
       <Footer />
-
     </div>
   );
 };
