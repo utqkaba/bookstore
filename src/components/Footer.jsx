091ef4413b0e9abc659be98dae10b41f40dc8433
utqkaba <utqkaba@gmail.com>
2023-12-31 19:44:53 +0300

    adding link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
+
   return (
     <footer className="bg-gray-900 p-4 text-white text-center bottom-0 w-full font-extralight text-sm">
       <p>&copy; BOOKSTORE </p>
       <div className="border-t border-gray-500 m-4"></div>
       <div className="grid grid-cols-2">
         <div className="font-extralight">2024 Made by <strong>Utku Kaba</strong></div>
-        <div>
-          github
+        <div className="grid place-items-center">
+          <a href="https://github.com/utqkaba" target="_blank" rel="noreferrer">  <FaGithub color="white" size="1.5rem" /> </a>
         </div>
       </div>
     </footer>
